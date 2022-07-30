77e10d9dfe2cbca6b332c196e900a0e28b962076
orekentaro <kntru0218gj@gmail.com>
2022-07-30 20:26:51 +0900

    shuusei

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, abort
-
+import os
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -10,13 +10,14 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
-# from config import LINE_BOT_API, HANDLER
 from modules.rezept_calculation import RezeptCalculation
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi("s8Q5GKHXNft77NFjOlxGey/w5DGMGIGPtI0VXGejD3mWZrDoP4jA1+NF7304hMKjWw+HUu8r3esiA09STedhHnmRB8tVPErjzWnO0YdhVynrDJHncdbh0WwAZQzrRS/ZJRWLOx1SG3xJRFhzvuE2dgdB04t89/1O/w1cDnyilFU=")
-handler = WebhookHandler("97606a458d1a1c8398558a6849e7dffd")
+ACCESS_TOKEN = os.environ["LINE_BOT_CHANNEL_SECRET"]
+CHANNEL_SECRET = os.environ["LINE_BOT_CHANNEL_TOKEN"]
+line_bot_api = LineBotApi(ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
 
 
 @app.route("/", methods=['GET'])
@@ -53,4 +54,5 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=port)
