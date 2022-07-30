ec5ad303890e460b1afb0e35b3176e5e8b8b9cca
orekentaro <kntru0218gj@gmail.com>
2022-07-30 21:24:23 +0900

    修正

--- src/app.py
+++ src/app.py
@@ -14,16 +14,19 @@ from linebot.models import (
 from modules.rezept_calculation import RezeptCalculation
 
 app = Flask(__name__)
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YOUR_CHANNEL_ACCESS_TOKEN", "")
-YOUR_CHANNEL_SECRET = os.environ.get("YOUR_CHANNEL_SECRET", "")
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YOUR_CHANNEL_ACCESS_TOKEN", "s8Q5GKHXNft77NFjOlxGey/w5DGMGIGPtI0VXGejD3mWZrDoP4jA1+NF7304hMKjWw+HUu8r3esiA09STedhHnmRB8tVPErjzWnO0YdhVynrDJHncdbh0WwAZQzrRS/ZJRWLOx1SG3xJRFhzvuE2dgdB04t89/1O/w1cDnyilFU=")
+YOUR_CHANNEL_SECRET = os.environ.get("YOUR_CHANNEL_SECRET", "97606a458d1a1c8398558a6849e7dffd")
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+
 @app.route("/", methods=['GET'])
 def test():
-    return 'OKだよdd'
-
+    try:
+        return 'OKだよdd'
+    except Exception as e:
+        print(f"えらーろぐ{e}")
 
 @app.route("/callback", methods=['POST'])
 def callback():
