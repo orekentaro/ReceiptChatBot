80eb72a9e9fdf2b8b206fd1d084f436a702242ff
orekentaro <kntru0218gj@gmail.com>
2022-09-01 15:52:32 +0900

    V2

--- src/modules/recept_calculation.py
+++ src/modules/recept_calculation.py
@@ -1,3 +1,7 @@
+from multiprocessing import reduction
+from unicodedata import name
+
+
 class ReceiptCalculation:
     def __init__(self, total: int, contact: int) -> None:
         self.total: int = total
@@ -8,10 +12,18 @@ class ReceiptCalculation:
         if total < contact:
             return 0, 0, 0
         percent: float = self._division(total, contact)
-        while self._check_percent(percent):
-            total -= 1
-            contact -= 1
-            percent = self._division(total, contact)
+        if not self._check_percent(percent):
+            while not self._check_percent(percent):
+                if self._check_percent_under(total, contact):
+                    break
+                total += 1
+                contact += 1
+                percent = self._division(total, contact)
+        else:
+            while self._check_percent(percent):
+                total -= 1
+                contact -= 1
+                percent = self._division(total, contact)
         return percent, total, contact
 
     def _division(self, denominator: int, molecule: int) -> float:
@@ -23,8 +35,12 @@ class ReceiptCalculation:
     def _check_percent(self, percent: float) -> bool:
         return 40.0 <= percent and percent != 0
 
+    def _check_percent_under(self, total: int, contact: int) -> bool:
+        percent = self._division(total+1, contact+1)
+        return 39.95 < percent and percent != 0
+
     def _parse_percent(self, percent: float) -> str:
-        return_val = str(percent)[:4]
+        return_val = str(percent)[:5]
         if return_val == "100.":
             return_val = "100.0"
         return f"{return_val}%"
@@ -50,22 +66,46 @@ class ReceiptCalculation:
         return result_dict
 
     def serialization(self, result_dict: dict) -> str:
-        return_txt = "【計算結果】\n"
-        return_txt += f"削除数: {result_dict['reduction']}人\n"
-        return_txt += "\n"
-        return_txt += f"削除前: {result_dict['percent']}\n"
-        return_txt += f"削除後: {result_dict['result_percent']}\n"
-        return_txt += "\n"
-        return_txt += "\n"
-        return_txt += "--------------------\n"
-        return_txt += "🧐エビデンス🧐\n"
-        return_txt += "--------------------\n"
-        return_txt += "\n"
-        return_txt += "【削除前】\n"
-        return_txt += f"全数: {result_dict['total']}人\n"
-        return_txt += f"CL数: {result_dict['contact']}人\n"
-        return_txt += "\n"
-        return_txt += "【削除後】\n"
-        return_txt += f"全数: {result_dict['result_total']}人\n"
-        return_txt += f"CL数: {result_dict['result_contact']}人"
+        if result_dict[reduction] >= 0:
+            return_txt = "【計算結果】\n"
+            return_txt += "40%を超えました🔥\n"
+            return_txt += "\n"
+            return_txt += f"削除数: {result_dict['reduction']}人\n"
+            return_txt += "\n"
+            return_txt += f"削除前: {result_dict['percent']}\n"
+            return_txt += f"削除後: {result_dict['result_percent']}\n"
+            return_txt += "\n"
+            return_txt += "\n"
+            return_txt += "--------------------\n"
+            return_txt += "🧐エビデンス🧐\n"
+            return_txt += "--------------------\n"
+            return_txt += "\n"
+            return_txt += "【削除前】\n"
+            return_txt += f"全数: {result_dict['total']}人\n"
+            return_txt += f"CL数: {result_dict['contact']}人\n"
+            return_txt += "\n"
+            return_txt += "【削除後】\n"
+            return_txt += f"全数: {result_dict['result_total']}人\n"
+            return_txt += f"CL数: {result_dict['result_contact']}人"
+        else:
+            return_txt = "【計算結果】\n"
+            return_txt += "40%を下回りました🔥\n"
+            return_txt += "\n"
+            return_txt += f"増加数: {result_dict['reduction'] * -1}人\n"
+            return_txt += "\n"
+            return_txt += f"増加前: {result_dict['percent']}\n"
+            return_txt += f"増加後: {result_dict['result_percent']}\n"
+            return_txt += "\n"
+            return_txt += "\n"
+            return_txt += "--------------------\n"
+            return_txt += "🧐エビデンス🧐\n"
+            return_txt += "--------------------\n"
+            return_txt += "\n"
+            return_txt += "【増加前】\n"
+            return_txt += f"全数: {result_dict['total']}人\n"
+            return_txt += f"CL数: {result_dict['contact']}人\n"
+            return_txt += "\n"
+            return_txt += "【増加後】\n"
+            return_txt += f"全数: {result_dict['result_total']}人\n"
+            return_txt += f"CL数: {result_dict['result_contact']}人"
         return return_txt
